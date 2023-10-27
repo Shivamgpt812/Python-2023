59a5cfbe67888b2a95fd992e96efc01e1b0fc1fb
Shivamgpt812 <144247614+Shivamgpt812@users.noreply.github.com>
2023-10-27 18:06:33 +0530

    Add files via upload

---
+++ A-7/2.py
@@ -0,0 +1,6 @@
+# Write a python script to print first 10 natural numbers
+i=1
+
+while i<11:
+    print(i)
+    i+=1
---
+++ A-7/4.py
@@ -0,0 +1,6 @@
+# Write a python script to print first 10 odd natural numbers
+i=1
+
+while i<=19:
+    print(i)
+    i+=2
